77b46829e7f3d856300238ec6b5fb808e332cc65
Hansen-G <guohansen0010@gmail.com>
2022-06-07 00:45:48 -0400

    Updates

---
+++ precache-manifest.2bc4b879ee37551e5860ab6961354c61.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5593fb6f6bf7a6ca3169d695796889d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9816d586b6c02e12736f",
+    "url": "/static/css/main.9d285d77.chunk.css"
+  },
+  {
+    "revision": "ccb4255a9afa5535029d",
+    "url": "/static/js/2.ab33c7f6.chunk.js"
+  },
+  {
+    "revision": "0aadc8a8405aeed76ae7e020b1d921e7",
+    "url": "/static/js/2.ab33c7f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9816d586b6c02e12736f",
+    "url": "/static/js/main.26c612a0.chunk.js"
+  },
+  {
+    "revision": "ad3971d54a6c92ea04ed",
+    "url": "/static/js/runtime-main.09ceff5b.js"
+  },
+  {
+    "revision": "b30698fd407f815496889ccfb376ca1a",
+    "url": "/static/media/arrow-dark.b30698fd.svg"
+  },
+  {
+    "revision": "873e41507cf45c1984c3cef30b76e867",
+    "url": "/static/media/arrow-light.873e4150.svg"
+  },
+  {
+    "revision": "9f76e244c286a0bad3a5ca5022d92f18",
+    "url": "/static/media/calligraphy.9f76e244.JPG"
+  },
+  {
+    "revision": "c145e541b3b5e803018950853a720cdd",
+    "url": "/static/media/carousel-arrow-dark.c145e541.svg"
+  },
+  {
+    "revision": "45b011613fc252a301fdb9428f450aa5",
+    "url": "/static/media/carousel-arrow-light.45b01161.svg"
+  },
+  {
+    "revision": "260eaeb79113751bff88730271b1d316",
+    "url": "/static/media/crafts.260eaeb7.jpg"
+  },
+  {
+    "revision": "18d9217b5751fdbba0c1bca5d7df0d7b",
+    "url": "/static/media/cursor.18d9217b.svg"
+  },
+  {
+    "revision": "c26549a4dad29dc154ed9833cb061421",
+    "url": "/static/media/cursor.c26549a4.cur"
+  },
+  {
+    "revision": "999b4b22c684d7abeba9828aca7ee316",
+    "url": "/static/media/hg-portfolio.999b4b22.jpg"
+  },
+  {
+    "revision": "7c64de5df15fa80f6977a31792c0a559",
+    "url": "/static/media/linkArrow.7c64de5d.svg"
+  },
+  {
+    "revision": "f67c0f0d73616ade0bc159eb3ee0e22d",
+    "url": "/static/media/painting.f67c0f0d.JPG"
+  },
+  {
+    "revision": "6c917456c5fee9fd7ca8309c9af28a79",
+    "url": "/static/media/singing.6c917456.jpg"
+  }
+]);
